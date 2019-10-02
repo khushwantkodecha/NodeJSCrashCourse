908326874f5fb3f8898432cde36bc6f4d33f60a0
Khushwant-CrewFoundry <khushwant.crewfoundry@gmail.com>
2019-10-02 19:35:38 +0530

    files deleted

--- coremodules.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-const filename = path.join(__filename);
-const basename = path.basename(filename);
-
-console.log("path", path);
-console.log("filename", filename);
-console.log("basename", basename);
--- index.js
+++
@@ -1,5 +0,0 @@
-const value = require("./square");
-
-console.log("value is ", value.b);
-console.log(__filename);
-console.log(__dirname);
